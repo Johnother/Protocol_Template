5230c39dd350d65c296f7c34f4ea0086407e1542
Lukas <lukas.beck@tu-dortmund.de>
2026-06-29 11:05:35 +0200

    v23 aufgesetzt

---
+++ v23/python_code/v23.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as const
+from scipy.optimize import curve_fit, fsolve
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+
+### functions ###
+
+### frequenzabhängigkeit ###
